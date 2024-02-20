551f0f229fdacfb348d21632ef6e71eaa3048606
Alejandro Ibarra <andresibarra@umariana.edu.co>
2024-02-19 23:38:23 -0500

    vista libro details, ejecutar npm install por si no funciona bootstrap

--- src/components/LibroDetails.js
+++ src/components/LibroDetails.js
@@ -3,6 +3,10 @@ import { useParams, Link } from 'react-router-dom';
 import { LibroContext } from '../context/LibroContext';
 import { LibroNotFound } from './LibroNotFound';
 import "../styles/styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Reply} from 'react-bootstrap-icons';
+
+
 
 export const LibroDetails = () => {
     const { libroId } = useParams();
@@ -13,9 +17,54 @@ export const LibroDetails = () => {
         return <LibroNotFound />;
     }
 
+    // Function to format to uppercase
+    //pasar al final como global
+    const convertToUppercase = (text) => {
+        return text.toUpperCase();
+    };
+
     return (
         <>
-            <br />
+
+
+            
+
+
+
+
+            <div className="card">
+                <div className="card-header">
+                    Detalles
+                    
+                        <Link to={`/Libros`} className="link-underline-opacity-0 ml-auto">
+                            Volver <Reply />
+                        </Link>
+
+
+                </div>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={libro.image} alt={libro.name} className="img-fluid rounded-start" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{convertToUppercase(libro.name)}</h5>
+                            <p className="card-text"><strong>Precio: </strong> ${libro.price}</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in fermentum nisl. Nulla facilisi. Vivamus eu aliquet nisl. Duis dapibus libero vitae velit ultricies, vel condimentum ex commodo. Sed at fermentum metus. Etiam eu arcu eu elit lacinia tempor. Integer ut neque bibendum, vestibulum turpis nec.</p>
+                            <p className="card-text"><strong className="text-info">Autor: </strong>{libro.author}</p>
+                            <p className="card-text"><strong>Año de publicación: </strong> {libro.publicationYear}</p>
+                            <p className="card-text"><strong>Categoría: </strong> {libro.category}</p>
+
+
+
+                           
+                           
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*            <br />
             <div className="libro-details-container">
                 <div className="libro-details-content">
                     <Link to={`/Libros`} className="volver-button">Volver</Link>
@@ -32,7 +81,7 @@ export const LibroDetails = () => {
                     <Link to={`/alquilar/${libro.id}`} className="ver-detalles-btn">Alquilar</Link>
                 </div>
             </div>
-            <br />
+            <br />*/}
         </>
     );
 };
