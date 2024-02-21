3af964bde742ab71d8c5c9f2e0bbdf414a055c5c
RobHoz95 <149828946+RobHoz95@users.noreply.github.com>
2024-02-20 21:38:55 -0500

    Update AlquilarLibro.js
    nuevo AlquilarLibro.js

--- src/components/AlquilarLibro.js
+++ src/components/AlquilarLibro.js
@@ -5,8 +5,6 @@ import { AlquilerContext } from "../context/AlquilerContext";
 import { LibroNotFound } from "../components/LibroNotFound";
 import { useNavigate } from "react-router-dom";
 import "../styles/styles.css";
-import {Reply} from "react-bootstrap-icons";
-
 
 export const AlquilarLibro = () => {
   const { libroId } = useParams();
@@ -14,8 +12,6 @@ export const AlquilarLibro = () => {
   const { agregarAlquiler } = useContext(AlquilerContext);
   const libro = libros.find((r) => r.id === libroId);
   const navigate = useNavigate();
-
-
 
   const [diasAlquiler, setDiasAlquiler] = useState("");
 
@@ -29,7 +25,9 @@ export const AlquilarLibro = () => {
       const nuevoAlquiler = {
         libroId: libro.id,
         libroName: libro.name,
-        diasAlquiler: parseInt(diasAlquiler*7)
+        libroImage: libro.image,
+        libroPrice: libro.price,
+        diasAlquiler: parseInt(diasAlquiler)
       };
       agregarAlquiler(nuevoAlquiler);
       navigate("/carrito");
@@ -48,48 +46,35 @@ export const AlquilarLibro = () => {
       <br />
       <div className="libro-details-container">
         <div className="libro-details-content">
-          <Link to={`/Libros/${libro.id}`} className="link-underline-opacity-0 ml-auto">
-            Volver <Reply/>
+          <Link to={`/Libros/${libro.id}`} className="volver-button">
+            Volver
           </Link>
           <h2>Alquilar</h2>
           <p className="">
             <strong> Libro a alquilar: </strong> {libro.name}
           </p>
           <label htmlFor="diasAlquiler">
-            <strong> Cuantas semanas desea alquilar:</strong>{" "}
+            <strong> Cuántos días desea alquilar:</strong>{" "}
           </label>
-          {/*          <input
+          <input
             type="number"
             id="diasAlquiler"
             value={diasAlquiler}
             onChange={handleDiasAlquilerChange}
             min="1"
-          />*/}
-
-          <select
-              className="form-select"
-              name="diasAlquiler"
-              id="diasAlquiler"
-              value={diasAlquiler}
-              onChange={handleDiasAlquilerChange}
-          >
-            <option value="">Selecciona</option>
-            <option value="1">1 Semana</option>
-            <option value="2">2 Semanas</option>
-            <option value="3">3 Semanas</option>
-          </select>
+          />
           <br/>
-          <button className="btn btn-outline-dark w-100" onClick={handleConfirmarClick}>Confirmar</button>
+          <button className="confirmar-button" onClick={handleConfirmarClick}>Confirmar</button>
         </div>
         <div className="libro-details-image-container">
           <img
-              className="libro-details-image"
-              src={libro.image}
-              alt={libro.name}
+            className="libro-details-image"
+            src={libro.image}
+            alt={libro.name}
           />
         </div>
       </div>
-      <br/>
+      <br />
     </>
   );
 };
